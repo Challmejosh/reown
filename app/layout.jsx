f81d7dc5415a2c16fe6d798b1191eec2acbf7479
Joshua Adediran <134891799+Challmejosh@users.noreply.github.com>
2025-05-06 09:14:05 -0700

    layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Context } from "@/components/context";
 import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 import Footer from "@/components/footer";
 
 const geistSans = Geist({
@@ -22,7 +23,9 @@ export const metadata = {
 
 export default async function RootLayout({children}) {
   const session = (await auth())
-
+  if(session){
+    redirect("/homepage")
+  }
   return (
     <html lang="en">
       <body
