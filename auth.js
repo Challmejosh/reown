fe7eec7b0dbee45f722d3914df56a4bca329d11d
Challmejosh <challmejosh@gmail.com>
2025-05-06 08:14:18 -0700

    done checking for further resources

--- auth.js
+++ auth.js
@@ -1,14 +1,9 @@
 import NextAuth from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
 import { writeClient } from "./sanity/lib/WriteClient";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
+  providers: [Google],
   callbacks: {
     async signIn({ user }) {
       if (!user?.email) {
